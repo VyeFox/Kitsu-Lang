fbcdec36487bffbd9a4cdc3da78f9d2fbf8aed94
VyeFox <violet.emmott@gmail.com>
2022-05-21 18:28:17 +0100

    new working stack for haskell only compiler (missing llvm-hs dependency)

---
+++ compiler/app/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import qualified Text.Megaparsec as MP
+import Control.Applicative ((<$), (<$>), (<|>))
+import Control.Monad (join, return)
+import qualified Text.Megaparsec.Char as MP
+import Data.Void ( Void )
+
+import Lib (example)
+
+main :: IO ()
+main = getLine >>= MP.parseTest example
---
+++ compiler/src/Lib.hs
@@ -0,0 +1,12 @@
+module Lib where
+
+import qualified Text.Megaparsec as MP
+import Control.Applicative ((<$), (<$>), (<|>))
+import Control.Monad (join, return)
+import qualified Text.Megaparsec.Char as MP
+import Data.Void ( Void )
+
+type Parser a = MP.Parsec Void String a
+
+example :: Parser String
+example = MP.string "example"
