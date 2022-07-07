4cd093376f4507f90bd74405605e19a352bf08e5
VyeFox <violet.emmott@gmail.com>
2022-07-07 15:08:25 +0100

    removed transformers

--- compiler/src/KitsuComponents.hs
+++ compiler/src/KitsuComponents.hs
@@ -35,10 +35,10 @@ instance Traversable TypeDefAttached where
 class (Monad m) => KitParseMonad m where
   pureTypeDefAttached :: TypeDefAttached a ->  m a
 
-parseIntegral :: (Num a, Ord e) => MP.ParsecT e String m a
+parseIntegral :: (Num a, Ord e) => MP.Parsec e String a
 parseIntegral = foldl (\ s dig -> s*10 + dig) 0 <$> MP.some digit
 
-digit :: (Num a, Ord e) => MP.ParsecT e String m a
+digit :: (Num a, Ord e) => MP.Parsec e String a
 digit = MP.label "0..9" $
   0 <$ MP.char '0' <|>
   1 <$ MP.char '1' <|>
@@ -51,7 +51,7 @@ digit = MP.label "0..9" $
   8 <$ MP.char '8' <|>
   9 <$ MP.char '9'
 
-hex :: (Num a, Ord e) => MP.ParsecT e String m a
+hex :: (Num a, Ord e) => MP.Parsec e String a
 hex = MP.label "0x.." $
   0 <$ MP.char '0' <|>
   1 <$ MP.char '1' <|>
@@ -70,7 +70,7 @@ hex = MP.label "0x.." $
   14 <$ MP.oneOf "eE" <|>
   15 <$ MP.oneOf "fF"
 
-escapedChar :: (Ord e) => MP.ParsecT e String m Char
+escapedChar :: (Ord e) => MP.Parsec e String Char
 escapedChar = MP.label "char" $
   '\n' <$ MP.string "\\n" <|>
   '\t' <$ MP.string "\\t" <|>
@@ -80,19 +80,19 @@ escapedChar = MP.label "char" $
   '\NUL' <$ MP.string "\\0" <|>
   MP.noneOf "\\\'\""
 
-sign :: (Num a, Ord e) => MP.ParsecT e String m (a -> a)
+sign :: (Num a, Ord e) => MP.Parsec e String (a -> a)
 sign = (id <$ MP.char '+') <|> (\x -> -x) <$ MP.char '-'
 
-textName :: (Ord e) => MP.ParsecT e String m String
+textName :: (Ord e) => MP.Parsec e String String
 textName = MP.label "prop name" $ (:) <$> startChar <*> MP.many restChar
   where
     startChar = MP.oneOf "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"
     restChar = MP.oneOf "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789"
 
-symbolicName :: (Ord e) => MP.ParsecT e String m String
+symbolicName :: (Ord e) => MP.Parsec e String String
 symbolicName = MP.label "var name" $ MP.some $ MP.oneOf "$%^&*-+=<>|?!:,/~"
 
-parseLiteral :: Ord e => MP.ParsecT e String m Literal
+parseLiteral :: (Ord e) => MP.Parsec e String Literal
 parseLiteral = MP.label "literal" $
   MP.try parseBool <|>
   MP.try parseByte <|>
@@ -108,19 +108,19 @@ parseLiteral = MP.label "literal" $
       parseByte = KitByte <$> ((\x' x -> 16*x' + x) <$> (MP.string "0x" *> hex) <*> hex)
       parseChar = KitChar <$> (MP.char '\'' *> escapedChar <* MP.char '\'')
 
-parseObjNotationLiteral :: Ord e => MP.ParsecT e String m Literal
+parseObjNotationLiteral :: (Ord e) => MP.Parsec e String Literal
 parseObjNotationLiteral = MP.label "object-notation-literal" $
   MP.try parseLiteral <|>
   parseRef -- reference for managing recursion in object notation
     where
       parseRef = KitClosureAddress <$> (MP.char '&' *> parseIntegral)
 
-parsePrimitive :: Ord e => MP.ParsecT e String m Literal -> MP.ParsecT e String m Primitive
+parsePrimitive :: (Ord e) => MP.Parsec e String Literal -> MP.Parsec e String Primitive
 parsePrimitive lit = MP.label "primitive" $
   MP.try (KitAsync <$> (MP.string "async" *> MP.space1 *> parseExpression lit)) <|>
   KitAtomic <$> (MP.string "atomic" *> MP.space1 *> parseExpression lit)
 
-parseClosure :: Ord e => MP.ParsecT e String m Literal -> MP.ParsecT e String m Expression
+parseClosure :: (Ord e) => MP.Parsec e String Literal -> MP.Parsec e String Expression
 parseClosure lit = MP.label "closure" $
   Closure "Object" <$> objectBody
     where
@@ -133,7 +133,7 @@ parseClosure lit = MP.label "closure" $
 -- *: Expression parser includes bracketed expressions.
 -- TODO: FUTURE: parse `$` syntax for formatted expressions.
 -- TODO: FUTURE: parse `do{...}` syntax for procedural logic.
-parseExpression :: Ord e => MP.ParsecT e String m Literal -> MP.ParsecT e String m Expression
+parseExpression :: (Ord e) => MP.Parsec e String Literal -> MP.Parsec e String Expression
 parseExpression lit = MP.label "expression" $
   value -- TODO: actually define this
     where
